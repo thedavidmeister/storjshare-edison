519e50837fc1a5f657c36dbb76709e9a92fc2965
David Meister <thedavidmeister@gmail.com>
2017-01-07 18:56:48 +1100

    add storjshare installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://github.com/Storj/storjshare-cli
+
 echo "=> Ensuring ~/.bashrc"
 touch ~/.bashrc
 
@@ -11,3 +13,9 @@ source ~/.bashrc
 
 echo "=> Installing node"
 nvm install 6.9.1
+
+echo "=> Installing storjshare"
+npm install -g storjshare-cli
+npm update -g storjshare-cli
+
+storjshare --help
