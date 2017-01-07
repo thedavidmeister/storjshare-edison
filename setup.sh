5ef299d0d30241634384e84b5a4debdff9eeb5c0
David Meister <thedavidmeister@gmail.com>
2017-01-07 18:52:27 +1100

    ensure bashrc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo "=> Ensuring ~/.bashrc..."
+touch ~/.bashrc
+
 echo "=> Installing nvm..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
