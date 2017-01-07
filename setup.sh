6509a67f0c2138e5d3d249466c8d0dc972d1c4bb
David Meister <thedavidmeister@gmail.com>
2017-01-07 18:55:39 +1100

    added node installation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "=> Ensuring ~/.bashrc..."
+echo "=> Ensuring ~/.bashrc"
 touch ~/.bashrc
 
-echo "=> Installing nvm..."
+echo "=> Installing nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+
+echo "=> Sourcing ~/.bashrc"
+source ~/.bashrc
+
+echo "=> Installing node"
+nvm install 6.9.1
